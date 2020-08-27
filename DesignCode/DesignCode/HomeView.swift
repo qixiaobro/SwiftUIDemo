1e610d29787eb3fc21269f713a2adeec2b3d2042
qixiaobro <982092690@qq.com>
2020-08-27 22:49:48 +0800

    add:11. Binding and Screen Size

---
+++ DesignCode/DesignCode/HomeView.swift
@@ -0,0 +1,36 @@
+//
+//  HomeView.swift
+//  DesignCode
+//
+//  Created by 朱晓东 on 2020/8/27.
+//  Copyright © 2020 xiaodongzhu. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                
+                Spacer()
+                
+                AvatarView(showProfile: $showProfile) //需要加上$
+            }
+            .padding(.horizontal)
+            .padding(.top, 30)
+            
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: .constant(false))
+    }
+}
