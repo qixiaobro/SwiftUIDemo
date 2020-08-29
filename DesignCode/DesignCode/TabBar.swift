3813d9cbe1a1490d0da83348b4520d1e50232a92
qixiaobro <982092690@qq.com>
2020-08-29 18:28:24 +0800

    add:19.Tab Bar and Test on Device

---
+++ DesignCode/DesignCode/TabBar.swift
@@ -0,0 +1,34 @@
+//
+//  TabBar.swift
+//  DesignCode
+//
+//  Created by 朱晓东 on 2020/8/29.
+//  Copyright © 2020 xiaodongzhu. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Certificates")
+            }
+        }
+//        .edgesIgnoringSafeArea(.)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            TabBar().previewDevice("iPhone 8")
+            TabBar().previewDevice("iPhone 11 Pro Max")
+        }
+    }
+}
