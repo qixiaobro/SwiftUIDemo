e7bbeac73f463c009a1a51586a4a37eb0e46d936
qixiaobro <982092690@qq.com>
2020-08-29 17:17:07 +0800

    add:17. Navigation Style and Passing Data

---
+++ DesignCode/DesignCode/UpdateDetail.swift
@@ -0,0 +1,34 @@
+//
+//  UpdateDetail.swift
+//  DesignCode
+//
+//  Created by 朱晓东 on 2020/8/29.
+//  Copyright © 2020 xiaodongzhu. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpdateDetail: View {
+    var update: Update = updateData[0]
+    
+    var body: some View {
+        List {
+            VStack {
+                Image(update.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                Text(update.text)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .navigationBarTitle(update.title)
+        }
+        .listStyle(PlainListStyle())
+    }
+}
+
+struct UpdateDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateDetail()
+    }
+}
