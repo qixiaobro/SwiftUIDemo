31ec6ae01ce7a199bf2d07145358b385de85e5f8
qixiaobro <982092690@qq.com>
2020-08-30 16:09:52 +0800

    add:31: Dismiss Gesture with Scale

--- DesignCode/DesignCode/CourseList.swift
+++ DesignCode/DesignCode/CourseList.swift
@@ -12,10 +12,11 @@ struct CourseList: View {
     @State var courses = courseData
     @State var active = false
     @State var activeIndex = -1
+    @State var activeView = CGSize.zero
     
     var body: some View {
         ZStack {
-            Color.black.opacity(active ? 0.5 : 0)
+            Color.black.opacity(Double(self.activeView.height / 500))
                 .animation(.linear)
                 .edgesIgnoringSafeArea(.all)
             
@@ -34,7 +35,8 @@ struct CourseList: View {
                                 show: self.$courses[index].show,
                                 course: self.courses[index],
                                 active: self.$active, index: index,
-                                activeIndex: self.$activeIndex
+                                activeIndex: self.$activeIndex,
+                                activeView: self.$activeView
                             )
                                 .offset(y: self.courses[index].show ? -geometry.frame(in: .global).minY : 0)//框架在屏幕中最小的y长度
                                 .opacity(self.activeIndex != index && self.active ? 0 : 1)
@@ -68,6 +70,7 @@ struct CourseView: View {
     @Binding var active: Bool
     var index: Int
     @Binding var activeIndex: Int
+    @Binding var activeView: CGSize
     
     var body: some View {
         ZStack(alignment: .top) {
@@ -128,6 +131,23 @@ struct CourseView: View {
                 .background(Color(course.color))
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                 .shadow(color: Color(course.color).opacity(0.3), radius: 20, x: 0, y: 20)
+                .gesture(
+                    show ?
+                    DragGesture().onChanged { value in
+                        guard value.translation.height < 300 else { return }
+                        guard value.translation.height > 0 else { return }
+                        self.activeView = value.translation
+                    }
+                    .onEnded { value in
+                        if self.activeView.height > 50 {
+                            self.show = false
+                            self.active = false
+                            self.activeIndex = -1
+                        }
+                        self.activeView = .zero
+                    }
+                    : nil
+                )
                 .onTapGesture {
                     self.show.toggle()
                     self.active.toggle()
@@ -139,7 +159,27 @@ struct CourseView: View {
                 }
         }
         .frame(height: show ? screen.height : 280)
+        .scaleEffect(1 - self.activeView.height / 1000)
+        .rotation3DEffect(Angle(degrees: Double(self.activeView.height / 10)), axis: (x: 0, y: 10.0, z: 0))
+        .hueRotation(Angle(degrees: Double(self.activeView.height)))
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+        .gesture(
+            show ?
+                DragGesture().onChanged { value in
+                    guard value.translation.height < 300 else { return }
+                    guard value.translation.height > 0 else { return }
+                    self.activeView = value.translation
+                }
+                .onEnded { value in
+                    if self.activeView.height > 50 {
+                        self.show = false
+                        self.active = false
+                        self.activeIndex = -1
+                    }
+                    self.activeView = .zero
+                }
+                : nil
+        )
         .edgesIgnoringSafeArea(.all)
     }
 }
