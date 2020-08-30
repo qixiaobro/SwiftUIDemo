0379f0577ffa72f779904ba073b2ddb0dade98dd
qixiaobro <982092690@qq.com>
2020-08-30 20:24:14 +0800

    add:37:URL Images

--- DesignCode/DesignCode/CourseStore.swift
+++ DesignCode/DesignCode/CourseStore.swift
@@ -31,6 +31,8 @@ class CourseStore: ObservableObject {
     @Published var courses: [Course] = courseData
     
     init() {
+        let colors = [#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1),#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1),#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1),#colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1),#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1),#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)]
+        
         getArray(id: "course") { (items) in
             items.forEach { (item) in
 
@@ -39,7 +41,7 @@ class CourseStore: ObservableObject {
                     subtitle: "ll",
                     image: #imageLiteral(resourceName: "Card2"),
                     logo: #imageLiteral(resourceName: "Logo1"),
-                    color: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1),
+                    color: colors.randomElement()!,
                     show: false))
             }
         }
